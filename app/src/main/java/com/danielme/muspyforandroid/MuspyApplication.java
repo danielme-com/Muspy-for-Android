9990f0f6274f9f7d9df3c95fea477ec00aa4cbac
Daniel Medina <danielme_com@yahoo.com>
2017-09-09 18:46:21 +0200

    Initialize Fabric with the debug-disabled crashlytics

--- app/src/main/java/com/danielme/muspyforandroid/MuspyApplication.java
+++ app/src/main/java/com/danielme/muspyforandroid/MuspyApplication.java
@@ -21,6 +21,7 @@ import android.app.Application;
 import android.content.Context;
 
 import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
 import com.squareup.leakcanary.LeakCanary;
 
 import io.fabric.sdk.android.Fabric;
@@ -43,7 +44,13 @@ public class MuspyApplication extends Application {
     }
     LeakCanary.install(this);
 
-    Fabric.with(this, new Crashlytics());
+    // Set up Crashlytics, disabled for debug builds
+    Crashlytics crashlyticsKit = new Crashlytics.Builder()
+        .core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
+        .build();
+
+    // Initialize Fabric with the debug-disabled crashlytics.
+    Fabric.with(this, crashlyticsKit);
 
     applicationDaggerComponent = DaggerApplicationDaggerComponent.builder()
         .applicationDaggerModule(new ApplicationDaggerModule(this))
